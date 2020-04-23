4a3ac0e99e07daeff6b6f532cb665f9cde3b66e0
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2020-04-23 22:22:05 +0200

    Components and syntaxfor making style global

---
+++ src/components/StartUp.svelte
@@ -0,0 +1,12 @@
+<!-- Compoenent with name prop -->
+<!-- MarkUp -->
+<h1>Svelte World!</h1>
+
+<p>Tutorial by <a href="https://www.youtube.com/watch?v=eLtmvV_SqqE&list=PLe30vg_FG4OSCWEp5r0n1CZ46RDfnC1U2" target="_blank">Bitfumes</a> YT channel.</p>
+
+<style>
+	h1 {
+		margin: 0;
+		padding: 0;
+	}
+</style>
